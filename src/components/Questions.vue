63e415ffb444a371e89e18dda0517210782972c8
Jason <jason.b.harris1@gmail.com>
2021-12-20 11:20:03 -0600

    modified app

---
+++ src/components/Questions.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="questions-ctr">
+    <div class="progress">
+      <div class="bar"></div>
+      <div class="status">1 out of 3 questions answered</div>
+    </div>
+    <div
+      class="single-question"
+      v-for="(question, qi) in questions"
+      :key="question.q"
+      v-show="questionsAnswered === qi"
+    >
+      <div class="question">{{ question.q }}</div>
+      <div
+        class="answers"
+        v-for="answer in question.answers"
+        :key="answer.text"
+      >
+        <div class="answer" @click.prevent="selectAnswer(answer.is_correct)">
+          {{ answer.text }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["questions", "questionsAnswered"],
+  emits: ["question-answered"],
+  methods: {
+    selectAnswer(is_correct) {
+      this.$emit("question-answered", is_correct);
+    },
+  },
+};
+</script>
+
+<style></style>
